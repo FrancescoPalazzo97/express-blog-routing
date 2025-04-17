6e7a5e2879d6c65bec703024cc5fd366c307e4e1
FrancescoPalazzo97 <francescopalazzo09@gmail.com>
2025-04-17 15:06:32 +0200

    [UPD] Added comments + 1st bonus

--- app.js
+++ app.js
@@ -2,16 +2,17 @@ const express = require(`express`);
 const app = express();
 const port = 3000;
 
-const posts = require(`./posts.js`);
+const posts = require(`./posts.js`); // Importo l'array
 
-const postsRouter = require(`./routers/router-post.js`)
+const postsRouter = require(`./routers/router-post.js`) // Importazione router
 
 app.use(express.static(`public`));
 
-app.use('/posts', postsRouter);
+app.use('/posts', postsRouter); // Utilizzo postsRouter per impostare le rotte
 
+// Rotta base
 app.get('/', (req, res) => {
-    res.json(posts);
+    res.json(posts); // Restituisco i post in formato JSON
 })
 
 app.listen(port, () => {
--- routers/router-post.js
+++ routers/router-post.js
@@ -1,28 +1,36 @@
 const express = require('express');
 const router = express.Router();
 
+// INDEX
 router.get('/', (req, res) => {
     res.send(`Lista post`);
 })
 
+// SHOW
 router.get('/:id', (req, res) => {
     res.send(`Ecco il post ${req.params.id}`);
 })
 
+
+// STORE
 router.post('/', (req, res) => {
     res.send(`Nuovo post creato`);
 })
 
+// UPDATE
 router.put('/:id', (req, res) => {
     res.send(`L'elemento ${req.params.id} è stato totalmente modificato`);
 })
 
+// MODIFY
 router.patch('/:id', (req, res) => {
     res.send(`L'elemento ${req.params.id} è stato parzialmente modificato`);
 })
 
+// DESTROY
 router.delete('/:id', (req, res) => {
     res.send(`L'elemento ${req.params.id} è stato eliminato`);
 })
 
+// ESPORTAZIONE ROUTER
 module.exports = router;
